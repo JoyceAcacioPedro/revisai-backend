ba200bed551983d925f368c6bdc8227ea3a6dba6
Joyce Acacio Pedro <joyceacaciopedro@gmail.com>
2026-08-22 15:06:28 +0200

    Fix: email via SMTP

--- RevisAI/settings.py
+++ RevisAI/settings.py
@@ -108,11 +108,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True 
+EMAIL_USE_TLS = False
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'joyceacaciopedro2005@gmail.com')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'rxby joqk qcgt enlj')
 DEFAULT_FROM_EMAIL = f'RevisAI <{EMAIL_HOST_USER}>'
+EMAIL_TIMEOUT = 10
 
 
 SIMPLE_JWT = {
