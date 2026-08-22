2b56c258a0e4946932f2849e63193f6d8eacda3c
Joyce Acacio Pedro <joyceacaciopedro@gmail.com>
2026-08-22 15:48:59 +0200

    Fix: email via SMTP

--- RevisAI/settings.py
+++ RevisAI/settings.py
@@ -111,9 +111,9 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
 EMAIL_USE_SSL = True 
 EMAIL_USE_TLS = False
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'joyceacaciopedro2005@gmail.com')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER', 'joyceacaciopedro2005@gmail.com')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 EMAIL_TIMEOUT = 10
 
 
