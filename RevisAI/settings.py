c89498ef9978522e79bd6dd0e56d12d26a7ec6e1
Joyce Acacio Pedro <joyceacaciopedro@gmail.com>
2026-08-26 23:31:03 +0200

    Fix CORS and settings

--- RevisAI/settings.py
+++ RevisAI/settings.py
@@ -123,8 +123,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Configurações de E-mail
 EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'django.core.mail.backends.smtp.EmailBackend')
 EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp-relay.brevo.com')
-EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 465))
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'False') == 'True'
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', 'True') == 'True'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'RevisAI <joyceacaciopedro2005@gmail.com>')
