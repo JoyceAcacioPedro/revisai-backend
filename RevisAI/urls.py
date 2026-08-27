de7bad27ce5f4fd2124f912354f8f0ff096f8e16
Joyce Acacio Pedro <joyceacaciopedro@gmail.com>
2026-08-27 09:57:44 +0200

    fix: brevo payload format

--- RevisAI/urls.py
+++ RevisAI/urls.py
@@ -1,27 +1,8 @@
-"""
-URL configuration for RevisAI project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
-from django.urls import path
 from django.http import JsonResponse
 from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 def create_admin_quick(request):
     User = get_user_model()
@@ -36,11 +17,10 @@ def create_admin_quick(request):
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),   # login
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # renovar token
+    
+    # Rota atualizada para bater com a chamada do Frontend (/api/user/login/)
+    path('api/user/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
     path('api/setup-admin/', create_admin_quick),
 ]
-
-
-
-
